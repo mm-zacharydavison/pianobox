107393e398d31818eca72469e10249ff061e4771
Zachary Davison <zachary.davison@meetsmore.com>
2025-01-18 09:45:09 +0100

    Make pianoteq MIDI installation more robust and happen before asking user for pianoteq location.

--- daemons/pianoteq/install.sh
+++ daemons/pianoteq/install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Copy MIDI preset.
+MIDI_PRESET="/home/patch/pianobox/daemons/pianoteq/pianobox.ptm"
+MIDI_PRESET_INSTALL_DIR="/home/patch/.local/share/Modartt/Pianoteq/MidiMappings"
+MIDI_PRESET_INSTALL_LOCATION="$MIDI_PRESET_INSTALL_DIR/pianobox.ptm"
+if [ ! -f "$MIDI_PRESET_INSTALL_LOCATION" ]; then
+  echo "  --> Installing pianobox.ptm MIDI mapping file..."
+  mkdir -p "$MIDI_PRESET_INSTALL_DIR"
+  cp "$MIDI_PRESET" "$MIDI_PRESET_INSTALL_LOCATION"
+fi
+
 # Check for Pianoteq installation.
 START_PIANOTEQ_SCRIPT="/home/patch/pianobox/daemons/pianoteq/start_pianoteq"
 if [ ! -f "$START_PIANOTEQ_SCRIPT" ]; then
@@ -9,12 +19,4 @@ if [ ! -f "$START_PIANOTEQ_SCRIPT" ]; then
     echo "$PIANOTEQ_EXECUTABLE" >> "$START_PIANOTEQ_SCRIPT"
     chmod +x "$START_PIANOTEQ_SCRIPT"
     echo "  --> start_pianoteq script created at $START_PIANOTEQ_SCRIPT"
-fi
-
-# Copy MIDI preset.
-MIDI_PRESET="/home/patch/pianobox/daemons/pianoteq/pianobox.ptm"
-MIDI_PRESET_INSTALL_LOCATION="/home/patch/.local/share/Modartt/Pianoteq/MidiMappings/pianobox.ptm"
-if [ ! -f "$MIDI_PRESET_INSTALL_LOCATION" ]; then
-  echo "  --> Installing pianobox.ptm MIDI mapping file..."
-  cp $MIDI_PRESET $MIDI_PRESET_INSTALL_LOCATION
 fi
