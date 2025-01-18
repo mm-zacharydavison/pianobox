9e555d8f9afdfdc01f8abbb7c34e7543189ea91e
Zachary Davison <zachary.davison@meetsmore.com>
2025-01-18 09:43:34 +0100

    Add installation of MIDI mapping file.

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 echo '1. Checking for Pianoteq installation...'
-START_PIANOTEQ_SCRIPT="/home/patch/pianobox/daemons/pianoteq/start_pianoteq"
-if [ ! -f "$START_PIANOTEQ_SCRIPT" ]; then
-    echo "start_pianoteq script not found. Please enter the absolute path of your Pianoteq executable:"
-    read PIANOTEQ_EXECUTABLE
-    echo "#!/bin/bash" > "$START_PIANOTEQ_SCRIPT"
-    echo "$PIANOTEQ_EXECUTABLE" >> "$START_PIANOTEQ_SCRIPT"
-    chmod +x "$START_PIANOTEQ_SCRIPT"
-    echo "start_pianoteq script created at $START_PIANOTEQ_SCRIPT"
-fi
+./daemons/pianoteq/install.sh
 
 echo '    --> Installing python requirements...'
 # We want the requirements installed into system python, because pisound will be using them too.
